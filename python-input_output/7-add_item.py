d4eba36d121c38adb6c9d20789ef8ee8282ebb2a
BYUSAA <byuussaa@gmail.com>
2024-08-06 02:03:09 +0100

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
+# script that adds all arguments to a Python list,
+# and then save them to a file
+"""
+    script that adds all arguments to a Python list,
+    and save them to a file
+"""
+
 import sys
-import os
 
-import json
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-def load_from_json_file(filename):
-    """Loads a Python object from a file using JSON representation."""
-    with open(filename, 'r') as f:
-        return json.load(f)
-
-import json
-
-def save_to_json_file(my_obj, filename):
-    """Saves a Python object to a file using JSON representation."""
-    with open(filename, 'w') as f:
-        json.dump(my_obj, f)
-
-
-# Import the necessary functions
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-filename = "add_item.json"
-
-# Load the existing data if the file exists, otherwise create an empty list
-if os.path.exists(filename):
-    items = load_from_json_file(filename)
-else:
-    items = []
-
-# Add all arguments to the list
-items.extend(sys.argv[1:])
-
-# Save the updated list back to the file
-save_to_json_file(items, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
