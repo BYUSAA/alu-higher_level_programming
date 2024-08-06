41902a702a3ff7e687374c7099c974db04eec8fe
BYUSAA <byuussaa@gmail.com>
2024-08-06 01:43:01 +0100

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-""" Load, add, save  """
-from sys import argv
-
-
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-""" Script that adds all arguments to a Python list,
-and then save them to a file """
-try:
-    add_i = load_from_json_file('add_item.json')
-    save_to_json_file(add_i + argv[1:], 'add_item.json')
-except Exception:
-    save_to_json_file(argv[1:], 'add_item.json')
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file, after each line containing a specific string
+    """
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
