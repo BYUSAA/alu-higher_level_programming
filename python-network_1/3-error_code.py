615e5fe17f7c5eca15e736c7913927d2a6582e39
BYUSAA <byuussaa@gmail.com>
2024-08-07 17:24:25 +0100

    Update 3-error_code.py

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""
-Module 3-error_code.py
-"""
-
-
+"""send request."""
 import urllib.request
 import urllib.error
-from sys import argv
+import sys
 
+
+def send_request():
+    """Send request."""
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as re:
+            xyz = re.read()
+            print(xyz.decode("utf-8"))
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
 
 if __name__ == "__main__":
-    req = urllib.request.Request(argv[1])
-    try:
-        with urllib.request.urlopen(req) as response:
-            print(response.read().decode('utf-8'))
-    except urllib.error.HTTPError as e:
-        print("Error code:", e.code)
+    send_request()
