f55e413b9d24c0e7fa6dcc8d6135ff12fb25d262
BYUSAA <byuussaa@gmail.com>
2024-08-07 17:25:05 +0100

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-Module 4-hbtn_status.py
-"""
+"""Python script that fetchs."""
+
 
 import requests
-
-
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    requ = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(requ.text)))
+    print("\t- content: {}".format(requ.text))
