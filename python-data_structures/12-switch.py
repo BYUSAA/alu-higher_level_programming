5e914aa5c0a53aada0343c77767dd7a30a701a23
BYUSAA <byuussaa@gmail.com>
2024-08-07 17:15:29 +0100

    Update 12-switch.py

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-def append_after(filename="", search_string="", new_string=""):
-    """Inserts a line of text to a file, after each line containing a specific string.
-    Args:
-        filename (str): The name of the file to append to.
-        search_string (str): The string to search for.
-        new_string (str): The string to append.
-    """
-    with open(filename, 'r+') as f:
-        lines = f.readlines()
-        f.seek(0)
-        for i, line in enumerate(lines):
-            f.write(line)
-            if search_string in line:
-                f.write(new_string)
-        f.truncate()
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
