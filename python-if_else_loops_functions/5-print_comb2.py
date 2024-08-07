35c46ce904685809e397af91e974a116eaa1fb56
BYUSAA <byuussaa@gmail.com>
2024-08-07 16:55:05 +0100

    Update 5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i <= 98:
-        print("{:02d}".format(i), end=", ")
+
+for num in range(100):
+    if num < 99:
+        print("{:02}, ".format(num), end='')
     else:
-        print(99)
+        print("{:02}".format(num))
+        
