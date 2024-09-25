b7a410e20548aa8fb8b477467470dc2f2555e00a
BYUUSAA <byuussaa@gmail.com>
2024-09-25 12:28:44 +0100

    Delete python-exceptions directory

--- python-exceptions/101-safe_function.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def safe_function(fct, *args):
-    from sys import stderr
-    try:
-        result = fct(*args)
-    except Exception as err:
-        result = None
-        stderr.write("Exception: {}\n".format(err))
-    return result
