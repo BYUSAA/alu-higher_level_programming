baf8bb3637465add2c10e99f902cb0b4d9121f6f
BYUSAA <byuussaa@gmail.com>
2024-09-26 07:02:02 -0700

    map

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,5 @@
-// 100-map.js
-const list = require('./100-data').list;
-
-const newList = list.map((item, index) => item * index);
-console.log(list);
-console.log(newList);
+#!/usr/bin/node
+const arr = require('./100-data.js').list;
+const newArr = arr.map((data, index) => index * data);
+console.log(arr);
+console.log(newArr);
