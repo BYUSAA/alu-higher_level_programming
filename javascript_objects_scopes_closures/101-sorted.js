49557f466337daa3404eff5655aefc07534c3c22
BYUSAA <byuussaa@gmail.com>
2024-09-27 04:09:44 -0700

    new updates

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,13 @@
-#!/usr/bin/node // Informs the system to use Node.js to run this script
+#!/usr/bin/node
 
-const inputDictionary = require('./101-data').dict; // Imports the `dict` property from the module '101-data'
+const inputDictionary = require('./101-data').dict;
 
-const reversedDictionary = {}; // Initializes an empty object to store the reversed dictionary
-for (const [key, value] of Object.entries(inputDictionary)) { // Loops through each key-value pair in the input dictionary
-  if (reversedDictionary[value] === undefined) { // Checks if the value is not yet a key in the reversed dictionary
-    reversedDictionary[value] = [key]; // Creates a new array with the current key as its first element
+const reversedDictionary = {};
+for (const [key, value] of Object.entries(inputDictionary)) {
+  if (reversedDictionary[value] === undefined) {
+    reversedDictionary[value] = [key];
   } else {
-    reversedDictionary[value].push(key); // Adds the current key to the existing array for this value
+    reversedDictionary[value].push(key);
   }
 }
-console.log(reversedDictionary); // Outputs the reversed dictionary to the console
+console.log(reversedDictionary);
