32cbef6132fa21eba57c301c0a00848b5327cd80
BYUSAA <byuussaa@gmail.com>
2024-09-27 01:44:09 -0700

    new updates

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,7 @@
-// 8-esrever.js
+#!/usr/bin/node
+
 exports.esrever = function (list) {
   const newList = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    newList.push(list[i]);
-  }
+  for (const x of list) newList.unshift(x);
   return newList;
 };
