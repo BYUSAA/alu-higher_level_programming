1a10c645111dd2c7363e225173e91b0188e3df30
BYUSAA <byuussaa@gmail.com>
2024-09-27 01:51:03 -0700

    updates

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
-// Define a function that is exported as a module, which takes a base as an argument.
-exports.converter = function (base) {
-  // Return a new function that takes a number as an argument and converts it to the specified base.
-  return (num) => {
-    // Use the toString() method to convert the number to a string representation in the specified base.
-    num.toString(base);
+exports.logMe = function (item) {
+  if (!this.count) {
+    this.count = 0;
   }
-}
+  console.log(`${this.count++}: ${item}`);
+};
