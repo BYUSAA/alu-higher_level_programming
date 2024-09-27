545f663a566874625b22eaf491c4b232317e15f7
BYUSAA <byuussaa@gmail.com>
2024-09-27 04:13:21 -0700

    new updates

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,12 @@
-#!/usr/bin/node // Informs the system to run the script using Node.js
-
-module.exports = class Square extends require('./5-square.js') { // Exports a class `Square` that extends a class from '5-square.js'
-
-  charPrint (c) { // Method `charPrint` that prints the square using the character `c`, or default if not provided
-    if (c === undefined) { // If no character `c` is provided
-      this.print(); // Call the inherited `print` method to print the square with default character
-    } else { 
-      for (let i = 0; i < this.height; i++) { // Loop through the height of the square
-        console.log(c.repeat(this.width)); // Print the character `c` repeated `width` times
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
       }
     }
   }
-}; // Close the class definition and export it
+};
