f79a8897a23a30edb647625eb567013f872d7003
BYUSAA <byuussaa@gmail.com>
2024-09-30 10:01:49 +0100

    new updates

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
+const http = require('http');
 const https = require('https');
+const url = require('url');
 
-function fetchStatusCode (url) {
-  https.get(url, (res) => {
+function fetchStatusCode (urlStr) {
+  const parsedUrl = url.parse(urlStr); // Parse the URL to detect the protocol
+
+  const protocol = parsedUrl.protocol === 'https:' ? https : http;
+
+  protocol.get(urlStr, (res) => {
     console.log(`code: ${res.statusCode}`);
   }).on('error', (err) => {
     console.error(err);
