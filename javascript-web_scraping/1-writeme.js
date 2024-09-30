00252d04ca414d31fe3a76800fc370c42fd0df4c
BYUSAA <byuussaa@gmail.com>
2024-09-30 09:52:35 +0100

    new updates

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,7 +1,12 @@
-#!/usr/bin/node // Informs the system to use Node.js to run this script
+#!/usr/bin/node
+const fs = require('fs').promises;
 
-const fs = require('fs'); // Imports the 'fs' (file system) module to handle file operations
+async function writeFileContent (filePath, content) {
+  try {
+    await fs.writeFile(filePath, content, 'utf8');
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (error) => { // Writes data from the third command-line argument to the file specified in the second argument
-  if (error) console.log(error); // Logs any error that occurs during the file write operation
-});
+writeFileContent(process.argv[2], process.argv[3]);
