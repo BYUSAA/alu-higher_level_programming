1a735091352846ab2b1d597591757f53ef1ef142
BYUSAA <byuussaa@gmail.com>
2024-09-30 10:07:46 +0100

    update

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,12 @@
-#!/usr/bin/node // Informs the system to use Node.js to run this script
+#!/usr/bin/node
+const axios = require('axios');
 
-const req = require('request'); // Imports the 'request' module to make HTTP requests
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`; // Constructs the URL using the film ID provided as the second command-line argument
-
-req.get(url, (err, res) => { // Makes a GET request to the constructed URL
-  if (err) { // Checks if there was an error during the request
-    console.error(err); // Logs the error to the console
-  } else { // If the request was successful
-    console.log(JSON.parse(res.body).title); // Parses the response body as JSON and logs the film's title to the console
-  }
-});
+axios.get(url)
+  .then(response => {
+    console.log(response.data.title);
+  })
+  .catch(error => {
+    console.error(error);
+  });
