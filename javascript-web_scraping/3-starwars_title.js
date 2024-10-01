b65a3df5dcf3203865fbca495530276cbc399867
BYUSAA <byuussaa@gmail.com>
2024-10-01 10:19:35 +0100

    updates

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,18 +1,11 @@
 #!/usr/bin/node
-const fetch = require('node-fetch');
+const req = require('request');
 
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
-
-fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data.title);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+req.get(url, (err, res) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(JSON.parse(res.body).title);
+  }
+});
